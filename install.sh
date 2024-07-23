a3da347ed783b16fd625cafcd2f9ead81253f3f1
Michele Menichetti <menichettimichele@gmail.com>
2024-07-23 16:18:42 +0200

    prova

--- install.sh
+++
@@ -1,6 +0,0 @@
-sudo cp ./services/bollettini-dotnet.service /etc/systemd/system/bollettini-dotnet.service
-sudo systemctl enable bollettini-dotnet.service
-sudo systemctl start bollettini-dotnet.service
-
-#Mostrare i log
-#sudo journalctl -fu kestrel-helloapp.service
